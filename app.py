7fa2946641a38f99e5173e6df2c073939b5508c4
Risheet Lenka <risheetlenka01@gmail.com>
2025-03-06 21:22:07 -0800

    implement new game functionality and update grid display; add event listeners for restart buttons

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, session, jsonify
 
 from os import environ
+from pprint import pp
 from secrets import token_hex
 
 load_dotenv()
@@ -14,7 +15,6 @@ app.secret_key = environ.get('SECRET_KEY', token_hex(16))
 def initialize_session():
     if 'board' not in session:
         # Initialize a new game
-        session['game_over'] = False
         session['score'] = 0
         session['moves'] = 0
         board = Board()  # Create Board instance
@@ -25,6 +25,7 @@ def initialize_session():
 @app.route('/')
 def index():
     # Pass current game state to the template
+    print(*[row for row in session['board']], sep='\n')
     return render_template('index.html', 
                           board=session['board'], 
                           score=session['score'], 
@@ -32,14 +33,27 @@ def index():
                           game_over=session['game_over'],
                           best_score = session.get('best_score', 0))
 
-@app.route('/new_game', methods=['POST'])
+@app.route('/update')
+def update():
+    raise NotImplementedError()
+
+@app.route('/move')
+def move():
+   raise NotImplementedError()
+
+@app.route('/new_game', methods=['GET'])
 def new_game():
-    session['game_over'] = False
-    session['best_score'] = session.get(max(session['score'], session['best_score']))
-    session['score'] = 0
-    session['moves'] = 0
+    print('New game')
+
     session['board'] = Board().grid  # Create a new board
-    return jsonify({'board': session['board'], 'score': session['score'], 'moves': session['moves']})
+
+    print(*[row for row in session['board']], sep='\n')
+
+    # session['score'] = 0
+    # session['moves'] = 0
+    # return jsonify({'board': session['board'], 'score': session['score'], 'moves': session['moves']})
+    return jsonify({'board': session['board']})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
 "use strict";
 
-function newGame() {}
+const restartButtons = document.querySelectorAll(".restart-button");
+restartButtons.forEach((button) => button.addEventListener("click", newGame));
+
+function newGame() {
+  console.log("newGame called");
+  fetch("/new_game")
+    .then((response) => response.json())
+    .then((data) => {
+      updateGrid(data.board);
+      document.querySelector(".game-message").classList.remove("game-over");
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+function updateGrid(board) {
+  console.log("updateGrid called");
+  const cells = document.querySelectorAll("td");
+  board.flat().forEach((value, index) => {
+    cells[index].textContent = value || "";
+    cells[index].className = value ? `tile-${value}` : "";
+  });
+}
